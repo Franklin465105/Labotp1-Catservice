3a545e70b797179e3eb28ca435322546be20ba4e
Franklin Esedebe <G00465105@atu.ie>
2026-09-16 14:22:58 +0100

    Set up the 1st microservice

---
+++ src/main/java/ie/atu/otp/week1/catservice/CatServiceLab1Application.java
@@ -0,0 +1,13 @@
+package ie.atu.otp.week1.catservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatServiceLab1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatServiceLab1Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/otp/week1/catservice/CatServiceLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.otp.week1.catservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatServiceLab1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
